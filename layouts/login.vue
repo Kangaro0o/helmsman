fe41005a681b9e9b91640697f8fff1196f740ec2
beverly <1262289000@qq.com>
2020-01-13 21:12:12 +0800

    logininin

---
+++ layouts/login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+       <footers></footers>
+    <div>我的博客导航栏在这里</div>
+    <nuxt/>
+   
+  </div>
+</template>
+
+<script>
+import Footer from '@/components/Footer'
+export default {
+    components: {
+    'footers': Footer
+  }
+}
+</script>
+
