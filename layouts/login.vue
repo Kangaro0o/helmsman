9f3b539462511aff3c85a8b69d80d98ef7611966
beverly <1262289000@qq.com>
2020-01-17 18:49:56 +0800

    登录页面修改1

--- layouts/login.vue
+++ layouts/login.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-       <log1></log1>
+       <log></log>
        
     <div>我的博客导航栏在这里</div>
     <nuxt/>
@@ -12,11 +12,11 @@
 
 <script>
 import Footer from '@/components/Footer'
-import log1 from '@/components/log1'
+import log from '@/components/log_in'
 export default {
     components: {
     'footers': Footer,
-    'log1': log1
+    'log': log
   }
 }
 </script>
