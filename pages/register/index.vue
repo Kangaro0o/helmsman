bd6ecba25f4b451072af69435cc527b6113a302c
beverly <1262289000@qq.com>
2020-02-02 16:12:04 +0800

    注册

---
+++ pages/register/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <reg></reg>
+  </div>
+</template>
+<script>
+import reg from '@/components/Reg'
+export default {
+  head() {
+    return {
+      title: "舵手商城 - 注册"
+    }
+  },
+  components: {
+    'reg': reg
+  },
+  layout: 'login'
+  // page component definitions
+
+}
+</script>
