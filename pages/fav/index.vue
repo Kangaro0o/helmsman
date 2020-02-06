79f5f9ccbbd5b7c49e1a29ea24bb4865059e5b5c
beverly <1262289000@qq.com>
2020-02-06 23:19:20 +0800

    收藏页

---
+++ pages/fav/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <favor></favor>
+  </div>
+</template>
+<script>
+import favor from '@/components/favor'
+export default {
+  head() {
+    return {
+      title: "舵手商城 - 收藏商品"
+    }
+  },
+  components: {
+    'favor': favor
+  },
+  layout: 'default'
+  // page component definitions
+
+}
+</script>
