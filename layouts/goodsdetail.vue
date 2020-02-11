057d1ffad5756dbb317a25796b228fc71487d89e
zmz <1558821214@qq.com>
2020-02-11 15:36:10 +0800

    商品详情页

---
+++ layouts/goodsdetail.vue
@@ -0,0 +1,29 @@
+<template>
+    <div >
+        <topbar></topbar>
+        <topheader></topheader>
+        <nuxt/>
+        <footers class="footercss"></footers>
+       
+    </div>
+</template>
+<script>
+    import '@/assets/reset.css'
+    import Footer from '@/components/Footer'
+    import TopBar from '@/components/TopBar'
+    import TopHeader from '@/components/TopHeader'
+    export default {
+        components: {
+            'footers':Footer,
+            'topbar':TopBar,
+            'topheader':TopHeader
+        }
+    }
+
+</script>
+<style >
+.footercss{
+    float:left;
+    padding-left:10%;
+}
+</style>
